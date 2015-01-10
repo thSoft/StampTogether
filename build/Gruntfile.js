918f9f596cf7ff91eb55f49d6a26023a1ca60dd1
Dénes Harmath <harmathdenes@gmail.com>
2015-01-10 14:43:01 +0100

    Readd stylesheet support

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -13,6 +13,7 @@ module.exports = function(grunt) {
 
 	var sourceSiteFolder = sourceFolder + "site/";
 	var siteScripts = [ sourceSiteFolder + "**/*.ts" ];
+	var stylesheets = [ sourceSiteFolder + "**/*.less" ];
 	var site = sourceSiteFolder + siteBasename;
 
 	var targetFolder = "../target/";
@@ -24,6 +25,8 @@ module.exports = function(grunt) {
 	var compiledElmBasename = "elm.js";
 	var compiledElm = targetSiteFolder + compiledElmBasename;
 	var compiledSiteScripts = targetSiteFolder + compiledScriptsBasename;
+	var compiledStylesheetsBasename = "stylesheets.css";
+	var compiledStylesheets = targetSiteFolder + compiledStylesheetsBasename;
 	var compiledSite = targetSiteFolder + siteBasename;
 
 	grunt.initConfig({
@@ -39,6 +42,10 @@ module.exports = function(grunt) {
 			typescript_site : {
 				files : siteScripts,
 				tasks : [ "typescript:site" ]
+			},
+			less : {
+				files : stylesheets,
+				tasks : [ "less" ]
 			},
 			site : {
 				files : site,
@@ -69,6 +76,12 @@ module.exports = function(grunt) {
 				dest : compiledSiteScripts
 			}
 		},
+		less : {
+			build : {
+				src : stylesheets,
+				dest : compiledStylesheets
+			}
+		},
 		dom_munger : {
 			build : {
 				src : site,
@@ -79,7 +92,8 @@ module.exports = function(grunt) {
 						html :
 							'<script src="' + compiledElmBasename + '" type="text/javascript"></script>' +
 							'<script src="' + compiledDependenciesBasename + '" type="text/javascript"></script>' +
-							'<script src="' + compiledScriptsBasename + '" type="text/javascript"></script>'
+							'<script src="' + compiledScriptsBasename + '" type="text/javascript"></script>' +
+							'<link rel="stylesheet" href="' + compiledStylesheetsBasename + '" type="text/css" />'
 					}
 				}
 			}
